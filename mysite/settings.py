d6417ce90cc6be678558421ae09107810003043d
arashrezaei84work <145213492+arashrezaei84work@users.noreply.github.com>
2025-08-15 17:39:07 +0330

    first static created
    the first duc of static created in app_1

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+
+STATIC_ROOT =  BASE_DIR / 'staticfiels'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
