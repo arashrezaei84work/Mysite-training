ad8c522885a5e03666db1fd65db5a22c6cfd0c9f
arashrezaei84work <145213492+arashrezaei84work@users.noreply.github.com>
2025-10-07 23:16:27 +0330

    test model
    it just test a model

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
     updated_date = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ('created_date',)
+        ordering = ('published_date',)
         # verbose_name = ????
         #verbose_name_plural = ????
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 # Create your views here.
 
 def blog_view(request):
-    return render(request, "blog/blog-home.html")
+    posts = Post.objects.filter(status=1)
+    context = {
+        'posts' : posts
+    }
+    return render(request, "blog/blog-home.html", context)
+
 
 def blog_detail(request):
     return render (request, "blog/blog-single.html")
 
-def test(request):
-    posts = Post.objects.all()
+def test(request, id):
+    # post = Post.objects.get(id=id)
+    post = get_object_or_404(Post, pk=id)
     context = {
-        'posts' : posts
+        'post' :  post
     }
     return render(request, "test.html", context) 
