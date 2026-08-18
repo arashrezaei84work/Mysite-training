451024f2c3a18a6614c6b5853360201575d9c59f
arashrezaei84work <145213492+arashrezaei84work@users.noreply.github.com>
2026-08-19 00:44:34 +0330

    edit templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -33,11 +33,10 @@ urlpatterns = [
     path("", include("app_1.urls")),
     path("blog/", include("blog.urls")),
     path('accounts/',include('accounts.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-     name='django.contrib.sitemaps.views.sitemap'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap'),
     path('robots.txt', include('robots.urls')),
     path('summernote/', include('django_summernote.urls')),
-     path('captcha/', include('captcha.urls')),
+    path('captcha/', include('captcha.urls')),
 
     
 ]
